46423fa130967d4d655fe67ad253d92cb847b92b
chaudharisunny <chaudharisunny6@gmail.com>
2026-09-12 13:06:38 +0530

    update code

--- dist/config/db.js
+++ dist/config/db.js
@@ -1,11 +1,6 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.pool = void 0;
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
 const pg_1 = require("pg");
 exports.pool = new pg_1.Pool({
     user: process.env.DB_USER,
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,3 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 import { Pool } from "pg";
 
 export const pool = new Pool({
