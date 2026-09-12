5dd6373590c4cc69d046d1b329d39e41825f7d1e
chaudharisunny <chaudharisunny6@gmail.com>
2026-09-12 13:16:45 +0530

    update code

--- dist/app.js
+++ dist/app.js
@@ -3,8 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
+require("dotenv/config");
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const PORT = 5000;
